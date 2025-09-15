0f76b365c64408c391428e532cfebc649ab5eb1f
Nikhil Nagpure <nikhilnagpure203@gmail.com>
2025-09-15 18:31:53 +0530

    hi

--- app/(public)/about-us/page.jsx
+++ app/(public)/about-us/page.jsx
@@ -1,4 +1,6 @@
-import { Link } from 'next/link';
+'use client'
+
+import Link from 'next/link';
 export default function AboutUs() {
   return (
     <div className="min-h-screen bg-gray-50 py-12 px-4 sm:px-6 lg:px-8">
